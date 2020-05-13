14f429624a0d53837b28f71d6c6ac5803729a29e
harzzek <harzz3k@gmail.com>
2020-05-13 13:09:51 +0200

    Project start...

---
+++ src/main/java/group5/nordic/NordicApplication.java
@@ -0,0 +1,13 @@
+package group5.nordic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NordicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NordicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/group5/nordic/NordicApplicationTests.java
@@ -0,0 +1,13 @@
+package group5.nordic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NordicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
